5a47660cef4363eedbd00da0687c49d6a421fc40
Patrick Randell <prandell@student.unimelb.edu.au>
2024-01-21 17:43:11 -0800

    fix: type imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { RecoilRoot } from 'recoil'
 import { BrowserRouter } from 'react-router-dom'
 import 'react-vertical-timeline-component/style.min.css'
 
@@ -16,24 +15,22 @@ import {
 
 const App: React.FC = () => {
   return (
-    <RecoilRoot>
-      <BrowserRouter basename="/">
-        <div className="relative z-0 bg-primary">
-          <div className="bg-primary bg-cover bg-no-repeat bg-center">
-            <Navbar />
-            <Hero />
-          </div>
-          <About />
-          <Experience />
-          <Tech />
-          <Works />
-          <div className="relative z-0">
-            <Contact />
-            <StarsCanvas />
-          </div>
+    <BrowserRouter basename="/">
+      <div className="relative z-0 bg-primary">
+        <div className="bg-primary bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
         </div>
-      </BrowserRouter>
-    </RecoilRoot>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,6 @@
 declare module 'glslify'
 declare module 'react-vertical-timeline-component'
 declare module '*.png'
+declare module '*.vert'
+declare module '*.frag'
 declare module 'maath/random/dist/maath-random.esm'
-declare module 'vite-plugin-glsl/ext'
