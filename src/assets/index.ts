dabc780a72c6557fdd2e034f6f0decca75d88b59
Patrick Randell <prandell@student.unimelb.edu.au>
2024-01-21 17:21:39 -0800

    feat: commit assets

---
+++ src/assets/index.ts
@@ -0,0 +1,25 @@
+import github from './github.png'
+import MenuIcon from './icons/menu.tsx'
+import CloseIcon from './icons/close.tsx'
+
+import rcomics from './rcomics.png'
+import live from './live.png'
+
+import home from './illuminate/home.png'
+import login from './illuminate/login.png'
+import map from './illuminate/map.png'
+import nws from './illuminate/nws.png'
+import risk from './illuminate/risk.png'
+
+export {
+  github,
+  live,
+  MenuIcon,
+  CloseIcon,
+  rcomics,
+  home,
+  login,
+  map,
+  nws,
+  risk
+}
