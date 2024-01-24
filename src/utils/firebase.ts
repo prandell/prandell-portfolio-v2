7d3cb1123218029c23c3f38d1364f9abcbee5b30
Patrick Randell <prandell@student.unimelb.edu.au>
2024-01-24 12:04:14 -0800

    fix: hide key

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -3,7 +3,7 @@ import { getFunctions, httpsCallable } from 'firebase/functions'
 
 //Prandell portfolio Web App Configuration
 const firebaseConfig: FirebaseOptions = {
-  apiKey: 'AIzaSyCp6x58mB4Fs7BY_yjNfiCiyhF-QtQXWIg',
+  apiKey: process.env.VITE_APP_FIREBASE_API_KEY,
   authDomain: 'prandell-portfolio.firebaseapp.com',
   projectId: 'prandell-portfolio',
   storageBucket: 'prandell-portfolio.appspot.com',
