57a3aa0f19ee32245da227ba761dba8c6570d778
Patrick Randell <prandell@student.unimelb.edu.au>
2024-01-24 11:04:43 -0800

    fix: build errors

--- src/components/ChatWindow/ChatMessage.tsx
+++ src/components/ChatWindow/ChatMessage.tsx
@@ -7,7 +7,10 @@ export interface IChatMessage {
   loading?: boolean
 }
 
-const MessageFill: React.FC<IChatMessage> = ({ message, isPb, loading }) => {
+const MessageFill: React.FC<Omit<IChatMessage, 'isPb'>> = ({
+  message,
+  loading
+}) => {
   return (
     <p className="flex w-fit max-w-[80%] justify-end bg-secondary py-3 px-3 rounded-2xl">
       {loading ? <TypingDots /> : `${message}`}
@@ -22,12 +25,12 @@ const ChatMessage: React.FC<IChatMessage> = ({ message, isPb, loading }) => {
         isPb ? 'left' : 'end'
       } items-center`}
     >
-      {!isPb && <MessageFill isPb={isPb} message={message} loading={loading} />}
+      {!isPb && <MessageFill message={message} loading={loading} />}
 
       <p className="flex text-center justify-center items-center bg-amber-400 rounded-full w-[42px] h-[42px]">
         {isPb ? '\uD83E\uDD16' : '\uD83D\uDC7E'}
       </p>
-      {isPb && <MessageFill isPb={isPb} message={message} loading={loading} />}
+      {isPb && <MessageFill message={message} loading={loading} />}
     </div>
   )
 }
